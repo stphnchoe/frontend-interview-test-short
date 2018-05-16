70c89a69532e9b943b12b6d434c9832bd4c6c13d
Stephen Choe <stphnchoe@gmail.com>
2018-05-16 11:55:03 -0700

    refactors event handler to select form in div with id=search

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
-  $('form').on('submit', function(e) {
+  $('#search').on('submit', 'form', function(e) {
     let date = new Date();
     let $input = $(e.currentTarget).find('input').first();
     let $post = $('<p></p>').text(`${$input.val()} - ${date.getSeconds()}`);
 
+    console.log('this is this', this);
+    console.log('this is target', e.currentTarget);
     e.preventDefault();
     $('#page').prepend($post);
     
